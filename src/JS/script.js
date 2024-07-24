13a593d583b8c623054a129fddf4e1004a160ba4
Reza salehi <reza.sl.rb5@gmail.com>
2024-07-25 01:11:15 +0330

    finish

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -121,6 +118,8 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 //Parent class
 class Workout {
+  clicks = 0;
+
   date = new Date();
 
   //tip:
@@ -136,17 +135,46 @@ class Workout {
     this.distance = distance; // in km
     this.duration = duration; //in min
   }
+  _setDescription() {
+    //tip:
+    //good thing is the array are ZERO base!
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    //new:
+    //cause the getMonth method are zero base , we can the the exacly index from months Array!!!
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    }${this.date.getDay()}`;
+  }
+  _click() {
+    this.clicks++;
+    console.log(this.clicks);
+  }
 }
 
 //Child class
 //Running
 class Running extends Workout {
-  type = "cycling";
+  type = "running";
 
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPeac();
+    this._setDescription();
   }
 
   calcPeac() {
@@ -164,6 +192,7 @@ class Cycling extends Workout {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed() {
@@ -184,19 +213,24 @@ class App {
   //Set the instanse varibles AND private
   #map;
   #mapEvent;
-  #workout = [];
+  #workouts = [];
+  #mapZoomLevel = 13;
 
   constructor() {
     //tip:
     //We dont want to call this function manuelly
     //So  we put this here, because this function will imediatly call , right after the page loaded
+    //tip:these are the thing which run at the begening
     this._getPosition();
+
+    this._getLocaleStorage();
 
     //tip:
     //We are using AGAING because in Eventlistener , THIS keyword pointing to the DOM element(form)
     form.addEventListener("submit", this._newWorkout.bind(this));
 
     inputType.addEventListener("change", this._toggleElevationField);
+    containerWorkouts.addEventListener("click", this._goToPopup.bind(this));
   }
 
   //Adding capsulation Methods
@@ -234,7 +268,7 @@ class App {
 
     //new:
     //third party library:
-    this.#map = L.map("map").setView(coords, 15);
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
     //Map IS OUR OBJECT WHICH WE'R interested on
 
     //this is Map style
@@ -256,6 +290,9 @@ class App {
 
     //tip: usnig bind
     this.#map.on("click", this._showForm.bind(this));
+
+    //rendering after the load
+    this.#workouts.forEach((work) => this._renderingWorkoutMarker(work));
   }
 
   //show from
@@ -266,6 +303,20 @@ class App {
     form.classList.remove("hidden");
     //jump in the form
     inputDistance.focus();
+  }
+
+  //Hide the form + clear the form
+  _hideForm() {
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        "";
+    //For hidde the class imediatly , we do this , because it have An animation
+    form.style.display = "none";
+    form.classList.add("hidden");
+    //put back  the display after 1 second
+    setTimeout(() => (form.style.display = "grid"), 1000);
   }
 
   //toggle field
@@ -299,8 +350,8 @@ class App {
     //the type element in HTML have type attribute , So check that
     if (type === "running") {
       //If workout is running , create the running object
-      const cadence = +inputCadence.value;
-
+      let cadence = +inputCadence.value;
+      let duration = +inputDuration.value;
       if (
         //tip:Instant of tihs , simply write a helper function
         // if(!Number.isFinite(type) || !Number.isFinite(duration) || !Number.isFinite(distance) || !Number.isFinite(cadence)) return
@@ -312,7 +363,7 @@ class App {
 
       //Add new workout to object array
       workout = new Running([lat, lng], distance, duration, cadence);
-      this.#workout.push(workout);
+      this.#workouts.push(workout);
       console.log(workout);
     }
 
@@ -327,25 +378,23 @@ class App {
 
       //Add new workout to object array
       workout = new Cycling([lat, lng], distance, duration, elevGain);
-      this.#workout.push(workout);
+      this.#workouts.push(workout);
       console.log(workout);
     }
 
     //Render workout as marker
-    this.renderingWorkoutMarker(workout);
+    this._renderingWorkoutMarker(workout);
     //Render workout on list
+    this._renderingWorkout(workout);
 
     //Hide the form + clear the form
-    inputCadence.value =
-      inputDistance.value =
-      inputDuration.value =
-      inputElevation.value =
-        "";
 
-    form.classList.add("hidden");
+    this._hideForm();
+
+    this._setLocaleStorage();
   }
 
-  renderingWorkoutMarker(workout) {
+  _renderingWorkoutMarker(workout) {
     //         //Adding new markers
     //         //For more option I ahve to use the documentation
     L.marker(workout.coords)
@@ -363,8 +412,116 @@ class App {
         )
       )
       .openPopup()
-      .setPopupContent("WorkOut");
+      .setPopupContent(
+        `${workout.type === "running" ? "🏃" : "🚴"} ${workout.description}`
+      );
   }
+  _renderingWorkout(workout) {
+    //tip:
+    //Using the part which is Common
+    let html = `
+    <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${
+              workout.type === "running" ? "🏃" : "🚴"
+            }</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>
+    `;
+    //tip: toFixed is for rounding Number to 1 decimal place
+
+    if (workout.type === "running") {
+      html += `
+       <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+      `;
+    }
+    if (workout.type === "cycling") {
+      html += `<div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🏔️</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>`;
+    }
+    //WE need to insert html after form AND when a new one Created , the old one goin down
+    form.insertAdjacentHTML("afterend", html);
+  }
+
+  _goToPopup(e) {
+    let workoutEl = e.target.closest(".workout");
+    console.log(workoutEl);
+    if (!workoutEl) return;
+
+    let workout = this.#workouts.find(
+      (work) => work.id === workoutEl.dataset.id
+    );
+    console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      Animat: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    workout._click();
+  }
+
+  //new:
+  //Locale Storage api , is a simple one
+  //And accept 2 String
+  //1. The Name
+  //2. Second one we can use Json.stringify() ,which make everything to string
+  //tip:because we HAVE to use locale storage for very small amoung data
+  _setLocaleStorage() {
+    localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+  } //now we can see it in Application on inspect
+
+  //new:
+  //Getting the Storage
+  //Using JSON.parse() , for reurn the string to object
+
+  _getLocaleStorage() {
+    const data = JSON.parse(localStorage.getItem("workouts"));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    //tip:
+    //Good thing to using classes And functionality is this,that we now just use that method not copy or refactor All code
+    this.#workouts.forEach((work) => this._renderingWorkout(work));
+  }
+
+  //AN Public method
+  //which can clean the Local Storage
+
+  reset() {
+    localStorage.removeItem("workouts");
+    //and An method for raload the page
+    location.reload();
+  } //now we can can use this on map variable in code or console
 }
 
 const map = new App();
